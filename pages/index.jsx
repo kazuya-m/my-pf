070e45c405c60dac3fa4fb73af0f176d6c2b85c5
kazuya-m <kazuya.matsuo5@gmail.com>
2020-12-29 19:36:03 +0900

    add folders and modified header

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,15 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { Header } from '../components/header'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>KM</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Header />
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
