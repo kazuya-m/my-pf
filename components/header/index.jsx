863e6c4925ce20bf3042cadd9e74c5cadf14dee4
kazuya-m <kazuya.matsuo5@gmail.com>
2020-12-30 21:57:56 +0900

    fix header and added svgs

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -6,15 +6,19 @@ export function Header() {
     <header className={styles.header}>
       <div className="header-left">
         <Link href="/">
-          <h1>Kazuya Matuso</h1>
+          <a><h1>Kazuya Matuso</h1></a>
         </Link>
       </div>
       <div className={styles.header__menu}>
-        <ul className={styles.menu}>
-          <li className={styles.menu__list}>Profile</li>
-          <li className={styles.menu__list}>Works</li>
-          <li className={styles.menu__list}>Contact</li>
-          <li className={styles.menu__list}>GitHub</li>
+        <ul className={styles.menu__list}>
+          <li className={styles.menu__item}>Profile</li>
+          <li className={styles.menu__item}>Works</li>
+          <li className={styles.menu__item}>Contact</li>
+          <li className={styles.menu__item}>
+            <a href="https://github.com/kazuya-m" target="_blank" rel="noopener noreferrer">
+            <img src="/skills/github.svg" alt="GitHub Logo" className="logo" />
+            </a>
+          </li>
         </ul>
       </div>
     </header>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import { Layout } from '../layouts'
 import { Top } from '../components/top'
 import { Profile } from '../components/profile'
