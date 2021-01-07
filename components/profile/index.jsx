3a2053a3781404bcda12bcb8d91a41c5009a8f6a
kazuya-m <kazuya.matsuo5@gmail.com>
2021-01-07 10:04:00 +0900

    midified content wrapper and title

--- components/profile/index.jsx
+++ components/profile/index.jsx
@@ -36,39 +36,41 @@ export function Profile() {
 
   return (
     <div className={styles.wrapper}>
-      <h3>Profile</h3>
-      <ul className={styles.profile__list}>
-        <li className={styles.profile__item} onClick={() => handleClickedTab(aboutme)}>About Me</li>
-        <li className={styles.profile__item} onClick={() => handleClickedTab(skills)}>SKills</li>
-      </ul>
-      <div className={styles.content__wrapper}>
-        {displayedTab === aboutme ? (
-          <div className={styles.card__aboutme}>
-            <div className={styles.item__profileImage}>
-              image
+      <h2>Profile</h2>
+      <div className={`content`}>
+        <ul className={styles.profile__list}>
+          <li className={styles.profile__item} onClick={() => handleClickedTab(aboutme)}>About Me</li>
+          <li className={styles.profile__item} onClick={() => handleClickedTab(skills)}>SKills</li>
+        </ul>
+        <div className={styles.content__wrapper}>
+          {displayedTab === aboutme ? (
+            <div className={styles.card__aboutme}>
+              <div className={styles.item__profileImage}>
+                image
+              </div>
+              <div className={styles.item__description}>
+              <p>こんにちは。マツオ カズヤと申します。<br />
+                  4年目の29歳です。<br />
+                  主にテスト案件を担当していましたが、2020年からは開発・改修に携わる案件に参画しています。<br />
+                  自己学習はモダンフロントエンドを中心に行っており、<a href="https://roadmap.sh/frontend" target="_blank" rel="noopener noreferrer">フロントエンドロードマップ <i className="fas fa-external-link-alt"></i></a> を参考にしています。<br />
+                  React / Next.js / Firebase / JavaScript / TypeScript<br />
+                  他の成果物もこのサイトやgithub上で公開しています。
+                </p>
+              </div>
             </div>
-            <div className={styles.item__description}>
-            <p>こんにちは。マツオ カズヤと申します。<br />
-                4年目の29歳です。<br />
-                主にテスト案件を担当していましたが、2020年からは開発・改修に携わる案件に参画しています。<br />
-                自己学習はモダンフロントエンドを中心に行っており、<a href="https://roadmap.sh/frontend" target="_blank" rel="noopener noreferrer">フロントエンドロードマップ <i className="fas fa-external-link-alt"></i></a> を参考にしています。<br />
-                React / Next.js / Firebase / JavaScript / TypeScript<br />
-                他の成果物もこのサイトやgithub上で公開しています。
-              </p>
-            </div>
-          </div>
-        ) : (
-          <ul className={styles.skills__list}>
-            {skillset.map((items) => (
-              <li id={items.name} className={styles.skills__item}>
-                <img src={items.imgPath} className={styles.logo__skills}/>
-                <h3 className={`fontLight`}>{items.name}</h3>
-                <p className={`fontLight lighterColor`}>{items.exp}</p>
-              </li>
-            )
+          ) : (
+            <ul className={styles.skills__list}>
+              {skillset.map((items) => (
+                <li id={items.name} className={styles.skills__item}>
+                  <img src={items.imgPath} className={styles.logo__skills}/>
+                  <h3 className={`fontLight`}>{items.name}</h3>
+                  <p className={`fontLight lighterColor`}>{items.exp}</p>
+                </li>
+              )
+            )}
+            </ul>
           )}
-          </ul>
-        )}
+        </div>
       </div>
     </div>
   )
