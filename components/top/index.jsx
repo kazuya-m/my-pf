5828860676f151119a7ee140bcc9a63b4c5e1c5f
kazuya-m <kazuya.matsuo5@gmail.com>
2021-01-16 16:11:49 +0900

    midified top style

--- components/top/index.jsx
+++ components/top/index.jsx
@@ -3,7 +3,9 @@ import styles from './index.module.css'
 export function Top() {
   return (
     <div className={`main__wrapper ${styles.wrapper}`}>
-      <h1 className={styles.title}>HELLO WORLD.</h1>
+      <div className={`${styles.bg}`}>
+        <h1 className={styles.title}>HELLO WORLD.</h1>
+      </div>
     </div>
   )
 }
